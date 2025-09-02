c9744f9d80e58b84bef0362d21b2b4921ed0a39f
csmile-1006 <cykim1006@gmail.com>
2025-09-02 06:07:17 +0000

    change details in collect_900M

--- data_gen_scripts/collect_900M.sh
+++ data_gen_scripts/collect_900M.sh
@@ -3,7 +3,7 @@ ENV_NAME=${1}
 START_SEED=${2}
 SEED_PER_PROCESS=${3}
 LOOP_COUNT=${4}
-for j in $(seq ${START_SEED} $(( ${START_SEED} + ${SEED_PER_PROCESS} ))); do
+for j in $(seq ${START_SEED} $(( ${START_SEED} + ${SEED_PER_PROCESS} - 1 ))); do
     (
         for i in $(seq -w $(( j * ${LOOP_COUNT} )) $(( (j + 1) * ${LOOP_COUNT} - 1 ))); do
             python generate_manipspace.py --seed=$i --env_name=${ENV_NAME} --save_path=/mnt/changyeon/corl2025/ogbench/${ENV_NAME}-play-1B-v0/${ENV_NAME}-play-v0-$i.npz --num_episodes=1000 --max_episode_steps=1001 --dataset_type=play;
